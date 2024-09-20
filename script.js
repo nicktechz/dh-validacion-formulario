9b0e496acc68a414e4a93909075fc2f060d7f241
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-09-20 18:50:15 -0500

    Added validation for the first name input

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 // LLAMAMOS LOS ELEMENTOS DEL DOM QUE SON NECESARIOS
-const form = document.getElementById('formActualizacion');
-
-form.addEventListener('submit', (e) => {
+document.getElementById('formActualizacion').addEventListener('submit', (e) => {
   e.preventDefault(); // PREVIENE QUE SE ACTUALICE LA PÁGINA
+  formValidation();
 });
 
-function formValidation() {
-  const firstName = document.getElementById('firstName');
-  const surName = document.getElementById('surName');
-  const numeroDocumento = document.getElementById('numeroDocumento');
+function showError(elemento, mensaje) {
+  const elementoError = document.getElementById(`error${elemento.id}`);
+  elementoError.innerHTML = mensaje;
+  elementoError.style.display = 'block';
 }
-formValidation();
+
+function hideError(elemento) {
+  const elementoError = document.getElementById(`error${elemento.id}`);
+  elementoError.innerHTML = '';
+  elementoError.style.display = 'none';
+}
+
+function formValidation() {
+  firstNameValidation();
+}
+
+function firstNameValidation() {
+  const firstName = document.getElementById('firstName');
+  if (firstName.value === '') {
+    showError(firstName, 'Este campo es requerido');
+  } else if (firstName.value.length > 0 && firstName.value.length < 3) {
+    showError(firstName, 'El nombre debe contener al menos 3 caracteres');
+  } else {
+    hideError(firstName);
+  }
+}
