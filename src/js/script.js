9a59a9f8473654dffa37704964540f83435bc0fc
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-09-21 08:27:13 -0500

    Added changes to the script files

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
 import {
+  actividadLaboral,
+  age,
+  email,
+  firstName,
+  formacionAcademica,
+  idNumber,
+  surName,
+  terms,
+  tipoDocumento,
+} from './utils/DOM.js';
+import {
   firstNameValidation,
   surNameValidation,
   identificationNumberValidation,
@@ -13,7 +24,16 @@ document.getElementById('formActualizacion').addEventListener('submit', (e) => {
   e.preventDefault(); // PREVIENE QUE SE ACTUALICE LA PÁGINA
   const isValid = formValidation();
   if (isValid) {
-    console.log('Listo para ser enviado');
+    console.log('Listo para ser enviado' + '\n');
+    console.log(`Nombre: ${firstName.value}` + '\n');
+    console.log(`Apellido: ${surName.value}` + '\n');
+    console.log(`Tipo de documento: ${tipoDocumento.value}` + '\n');
+    console.log(`Número Documento: ${idNumber.value}` + '\n');
+    console.log(`Edad: ${age.value}` + '\n');
+    console.log(`Correo electrónico: ${email.value}` + '\n');
+    console.log(`Actividad Laboral: ${actividadLaboral.value}` + '\n');
+    console.log(`Formacion Academica: ${formacionAcademica.value}` + '\n');
+    console.log(`Terminos: ${terms.value}` + '\n');
   } else {
     console.warn('El formulario contiene algunos errores');
   }
@@ -27,6 +47,5 @@ function formValidation() {
   emailVerification();
   termsValidation();
   const formValidation = isFormValid();
-  console.log();
   return formValidation;
 }
--- src/js/utils/DOM.js
+++ src/js/utils/DOM.js
@@ -1,8 +1,21 @@
 const firstName = document.getElementById('firstName');
 const surName = document.getElementById('surName');
+const tipoDocumento = document.getElementById('tipoDocumento');
 const idNumber = document.getElementById('idNumber');
 const age = document.getElementById('age');
 const email = document.getElementById('email');
 const terms = document.getElementById('terms');
+const formacionAcademica = document.getElementById('formacionAcademica');
+const actividadLaboral = document.getElementById('formacionAcademica');
 
-export { firstName, surName, idNumber, age, email, terms };
+export {
+  firstName,
+  surName,
+  tipoDocumento,
+  idNumber,
+  age,
+  email,
+  terms,
+  formacionAcademica,
+  actividadLaboral,
+};
